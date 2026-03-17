02ef257c7670d327b341376fcfff2324e05708d4
mounika <189mounika.b@gmail.com>
2026-03-17 10:07:10 +0530

    *    ***   *****  ******* *********

--- pattern8.py
+++ pattern8.py
@@ -1,6 +1,3 @@
 n=5
-for i in range (n,0,-1):
-    print(" "*(n-i),end=" ")
-    for j in range(1,i+1):
-        print("*",end="")
-    print()
+for i in range (1,n+1):
+    print(" "*(n-i) + "*"*(2*i-1))
