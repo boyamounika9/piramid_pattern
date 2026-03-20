82375ef1d1a0d76399a71cd7e08957c1eedc9a52
mounika <189mounika.b@gmail.com>
2026-03-20 22:25:45 +0530

    vowel: 4 consonant: 3

--- pro1.py
+++ pro1.py
@@ -1,2 +1,10 @@
 name="mounika"
-
+vowel=0
+cons=0
+for i in name:
+    if i in ("a","e","i","o","u","A","E","O","U","I"):
+        vowel+=1
+    else:
+        cons+=1
+print("vowel:",vowel)
+print("consonant:",cons)
