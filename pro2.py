7df8a3ce4ace815f447429126ba93ed0d107254d
mounika <189mounika.b@gmail.com>
2026-03-20 23:16:16 +0530

    new

--- pro2.py
+++ pro2.py
@@ -1,3 +1,9 @@
 name="mounika"
-print(name[::-1])
-print(name)
+reverse=len(name)*" "
+for i in name:
+    for j in range(-1,-(len(name)),-1):
+        reverse[j]+=i
+        continue
+print(reverse)
+
+
