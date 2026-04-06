abad820148d454444835a9ef8f7d4b96ff23a106
mounika <189mounika.b@gmail.com>
2026-04-06 12:04:28 +0530

    updating

--- calculator using fun.py
+++ calculator using fun.py
@@ -20,32 +20,32 @@
 
 #using mul functions
 
-def add():
-    print(a+b)
-def sub():
-    print(a-b)
-def mul():
-    print(a*b)
-def div():
-    print(a/b)
-def mod():
-    print(a%b)
-def non():
-    print("not a valid symbol")
-def fun(a,c,b):
-    if c=="+":
-     add()
-    elif c=="-":
-     sub()
-    elif c=="*":
-     mul()
-    elif c=="/":
-     div()
-    elif c=="%":
-     mod()
-    else:
-     non()
-a=int(input())
-c=input()
-b=int(input())
-fun(a,c,b)
+# def add():
+#     print(a+b)
+# def sub():
+#     print(a-b)
+# def mul():
+#     print(a*b)
+# def div():
+#     print(a/b)
+# def mod():
+#     print(a%b)
+# def non():
+#     print("not a valid symbol")
+# def fun(a,c,b):
+#     if c=="+":
+#      add()
+#     elif c=="-":
+#      sub()
+#     elif c=="*":
+#      mul()
+#     elif c=="/":
+#      div()
+#     elif c=="%":
+#      mod()
+#     else:
+#      non()
+# a=int(input())
+# c=input()
+# b=int(input())
+# fun(a,c,b)
