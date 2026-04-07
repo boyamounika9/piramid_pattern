c460f2f9309b280809fe01425b3899e0e2abdf90
mounika <189mounika.b@gmail.com>
2026-04-07 23:40:02 +0530

    Create a parent class Shape and child classes Circle and Rectangle. Display area methods for both.

---
+++ Hierarchical Inheritance.py
@@ -0,0 +1,10 @@
+class Shape:
+    def circlearea(self):
+        print("area of circle")
+    def rectangle(self):
+        print("area of rectangle")
+class Circle(Shape):
+    def area1(self):
+        pass
+class Rectangle(Shape):
+    
