71135db14603af3963364e6f900cba167c622746
mounika <189mounika.b@gmail.com>
2026-04-07 23:41:42 +0530

    Write a program where one parent class is inherited by two different child classes with different behaviors.

--- Hierarchical Inheritance.py
+++ Hierarchical Inheritance.py
@@ -7,4 +7,10 @@ class Circle(Shape):
     def area1(self):
         pass
 class Rectangle(Shape):
-    
+    def area2(self):
+        pass
+a1=Circle()
+a2=Rectangle()
+a1.circlearea()
+a2.rectangle()
+
