311500d0881f85644f1536cac57e77d53a7ff1f4
mounika <189mounika.b@gmail.com>
2026-04-07 22:00:05 +0530

    opps inheritance

--- calculator using fun.py
+++ calculator using fun.py
@@ -1,58 +1,24 @@
 # using single fun
-# def fun(a,b,c):
-#     if b=="+":
-#         print(a+c)
-#     elif b=="-":
-#         print(a-c)
-#     elif b=="*":
-#         print(a*c)
-#     elif b=="/":
-#         print(a/c)
-#     elif b=="%":
-#         print(a%c)
-#     else:
-#         print("invalid symbol")
-# a=int(input("enter first num"))
-# b=input("enter operator")
-# c=int(input("enter second num"))
-# fun(a,b,c)
+def fun(a,b,c):
+    if b=="+":
+        print(a+c)
+    elif b=="-":
+        print(a-c)
+    elif b=="*":
+        print(a*c)
+    elif b=="/":
+        print(a/c)
+    elif b=="%":
+        print(a%c)
+    else:
+        print("invalid symbol")
+a=int(input("enter first num"))
+b=input("enter operator")
+c=int(input("enter second num"))
+fun(a,b,c)
 
 
 #using mul functions
-
-# def add():
-#     print(a+b)
-# def sub():
-#     print(a-b)
-# def mul():
-#     print(a*b)
-# def div():
-#     print(a/b)
-# def mod():
-#     print(a%b)
-# def non():
-#     print("not a valid symbol")
-# def fun(a,c,b):
-#     if c=="+":
-#      add()
-#     elif c=="-":
-#      sub()
-#     elif c=="*":
-#      mul()
-#     elif c=="/":
-#      div()
-#     elif c=="%":
-#      mod()
-#     else:
-#      non()
-# a=int(input())
-# c=input()
-# b=int(input())
-# fun(a,c,b)
-
-
-
-#storing output in one variable
 
 def add():
     print(a+b)
@@ -66,7 +32,7 @@ def mod():
     print(a%b)
 def non():
     print("not a valid symbol")
-def fun():
+def fun(a,c,b):
     if c=="+":
      add()
     elif c=="-":
@@ -82,4 +48,6 @@ def fun():
 a=int(input())
 c=input()
 b=int(input())
-fun()
+fun(a,c,b)
+
+
