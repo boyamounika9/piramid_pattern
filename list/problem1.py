3fbf7a8567962363a0b05be249c883c96d024bc7
mounika <189mounika.b@gmail.com>
2026-04-14 23:24:28 +0530

    Count how many even and odd numbers are in a list.

--- list/problem1.py
+++ list/problem1.py
@@ -8,3 +8,16 @@ print(sum(lis))
 # Find the maximum and minimum element in a list.
 print(min(lis))
 print(max(lis))
+
+# Count how many even and odd numbers are in a list.
+even=0
+odd=0
+for i in lis:
+    if i%2==0:
+        even+=1
+    else:
+        odd+=1
+print("even:",even)
+print("odd:",odd)
+
+
