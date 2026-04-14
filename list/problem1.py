c535faa485d36296fd0676f37c49e82a73a76125
mounika <189mounika.b@gmail.com>
2026-04-14 23:20:25 +0530

    Find the sum of all elements in a list. Find the maximum and minimum element in a list.

--- list/problem1.py
+++ list/problem1.py
@@ -1,4 +1,10 @@
 #Create a list of 10 numbers and print all elements.
-
 lis=[1,2,3,4,5,6,7,8,9,10]
 print(lis)
+
+#Find the sum of all elements in a list.
+print(sum(lis))
+
+# Find the maximum and minimum element in a list.
+print(min(lis))
+print(max(lis))
