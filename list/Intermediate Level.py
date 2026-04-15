74c481ac7641af7f3b822e3e51a9e2bf4a180c14
mounika <189mounika.b@gmail.com>
2026-04-15 12:09:47 +0530

    Count frequency of each element in a list.

--- list/Intermediate Level.py
+++ list/Intermediate Level.py
@@ -1,4 +1,36 @@
-# Find the second largest number in a list.
-list=[3,4,6,7,10,8,9,2,1]
-list.sort()
-print(list[-2])
+# # # Find the second largest number in a list.
+# list=[1,1,True,1]
+# # list.sort()
+# # print(list[-2])
+
+# # # Check if a list is a palindrome.
+# reversedlist=list[:]
+# for i in range(0,len(list)):
+#     reversedlist[-(i+1)]=list[i]
+# if list==reversedlist:
+#     print("palindrom")
+# else:
+#     print("not a palindrom")
+
+
+# # my_list = list(map(int,input("Enter the list items").split()))
+# # print(my_list)
+
+# # Merge two lists and sort them.
+# list1=[1,2,3,4]
+# list2=[9,8,7,6]
+# list3=list1+list2
+# list3.sort()
+# print(list3)
+
+# Find common elements between two lists.
+
+# list1=set([1,2,3,4])
+# list2=set([3,4,5,6,7])
+
+# print(list(list1 & list2))
+
+# Count frequency of each element in a list.
+list=[1,3,4,7,1,2,8,4,3]
+dic = {}
+
