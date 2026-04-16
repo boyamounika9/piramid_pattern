628f027bcf32ed1636f11d16b5ab8515f3fdd9ae
mounika <189mounika.b@gmail.com>
2026-04-16 14:14:45 +0530

    0 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 0

--- Patterns/square.py
+++ Patterns/square.py
@@ -41,3 +41,13 @@
 #         else:
 #             print("0",end=" ")
 #     print()
+
+
+# n=int(input())
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i+j==n+1:
+#             print("1",end=" ")
+#         else:
+#             print("0",end=" ")
+#     print()
