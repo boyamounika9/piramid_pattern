e2ea7c0c8cef3432bc5cc2f1140b9712eb2e90b6
mounika <189mounika.b@gmail.com>
2026-04-16 12:13:31 +0530

    while problem2

--- loops/whileloop.py
+++ loops/whileloop.py
@@ -1,8 +1,17 @@
-n=int(input())
-i=1
-count=0
-while n>=i:
-    if n%2==0:
-        count+=1
-    n-=1
-print(count)
+# n=int(input())
+# i=1
+# count=0
+# while n>=i:
+#     if n%2==0:
+#         count+=1
+#     n-=1
+# print(count)
+
+# n=int(input())
+# i=1
+# while n>=i:
+#     if n%2==0:
+#         print(n,end=" ")
+#     n-=1
+
+
