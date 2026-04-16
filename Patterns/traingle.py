90d34eb8ea17d022800e6797591da0bd4e1f4603
mounika <189mounika.b@gmail.com>
2026-04-16 14:26:50 +0530

    1 2 3 4 2 3 4 1 3 4 1 2 4 1 2 3

--- Patterns/traingle.py
+++ Patterns/traingle.py
@@ -3,3 +3,11 @@
 #     for j in range (1,i+1):
 #         print("*",end=" ")
 #     print()
+
+# n=int(input())
+# for i in range (1,n+1):
+#     for j in range (i,n+1):
+#         print(j,end=" ")
+#     for j in range (1,i):
+#         print(j,end=" ")
+#     print()
