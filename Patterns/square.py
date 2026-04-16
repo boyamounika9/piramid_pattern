a7c21cf1bdaf730962565e78f617c8fd848853b4
mounika <189mounika.b@gmail.com>
2026-04-16 14:01:56 +0530

    patten 5,6

--- Patterns/square.py
+++ Patterns/square.py
@@ -12,3 +12,14 @@
 #         else:
 #             print("o",end=" ")
 #     print()
+
+
+
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range (1,n+1):
+#         if i%2==0:
+#             print("1",end=" ")
+#         else:
+#             print("o",end=" ")
+#     print()
