af6acd851d6f267342c69caaa5294e24cd8644ce
mounika <189mounika.b@gmail.com>
2026-04-16 12:44:17 +0530

    promblem 10

--- loops/whileloop.py
+++ loops/whileloop.py
@@ -58,19 +58,23 @@
 #     i+=1
 # print(factorial)
     
-n=int(input())
-temp=n
-sum=0
-mul=1
-while n!=0:
-    digit=n%10
-    sum+=digit
-    n//=10
-while temp!=0:
-    digit=temp%10
-    mul*=digit
-    temp//=10
-if sum==mul:
-    print("spy")
-else:
-    print("no")
+
+#-----------spy------------
+# n=int(input())
+# temp=n
+# sum=0
+# mul=1
+# while n!=0:
+#     digit=n%10
+#     sum+=digit
+#     n//=10
+# while temp!=0:
+#     digit=temp%10
+#     mul*=digit
+#     temp//=10
+# if sum==mul:
+#     print("spy")
+# else:
+#     print("no")
+
+
