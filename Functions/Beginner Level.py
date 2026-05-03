f7d58fe6c74b77946802758fc42ae513b2406979
mounika <189mounika.b@gmail.com>
2026-05-03 22:08:49 +0530

    function 2

--- Functions/Beginner Level.py
+++ Functions/Beginner Level.py
@@ -1,3 +1,8 @@
-def greet(name):
-    print("HELLO,",name,"!")
-greet("mounika")
+# def greet(name):
+#     print("HELLO,",name,"!")
+# greet("mounika")
+
+def add(a,b):
+    return a+b
+
+print(add(2,3))
