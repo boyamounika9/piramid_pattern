e0c1c54a1dc8e350b564f6b2698ddcc3f94305f1
mounika <189mounika.b@gmail.com>
2026-05-03 23:18:13 +0530

    exception 5

--- Exception.py
+++ Exception.py
@@ -8,15 +8,15 @@
 #     print("sucess")
 
 
-try:
-    a=int(input())
-    b=int(input())
-    print(a/b)
-except ZeroDivisionError:
-    print("cant divisible by zero")
-except ValueError:
-    print("invalid input")
-except Exception as e:
-    print("unexpected error" ,e)
+# try:
+#     a=int(input())
+#     b=int(input())
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("cant divisible by zero")
+# except ValueError:
+#     print("invalid input")
+# except Exception as e:
+#     print("unexpected error" ,e)
 
     
