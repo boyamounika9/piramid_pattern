13d4c62959c5008669a5a7b22617e5ba6c8f64aa
mounika <189mounika.b@gmail.com>
2026-05-03 23:19:29 +0530

    Write a program that:
    takes two numbers
    divides them
    prints result using else block

--- Exception.py
+++ Exception.py
@@ -20,17 +20,17 @@
 #     print("unexpected error" ,e)
 
     
-try:
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
+# try:
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
     
-    result = a / b
+#     result = a / b
 
-except ZeroDivisionError:
-    print("Cannot divide by zero")
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
 
-except ValueError:
-    print("Invalid input")
+# except ValueError:
+#     print("Invalid input")
 
-else:
-    print("Result:", result)
+# else:
+#     print("Result:", result)
