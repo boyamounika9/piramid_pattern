6d683b8120bf7b7d6f7403299467712ee7ff125c
mounika <189mounika.b@gmail.com>
2026-05-03 23:57:01 +0530

    Write a program to count number of words in a file.

--- file Handling.py
+++ file Handling.py
@@ -2,13 +2,13 @@
 # count = len(file.readlines())
 # print("Number of lines:", count)
 # file.close
-try:
-    with open("f.txt", "r") as file:
-        data = file.read()
-        words = data.split()
-        count = len(words)
+# try:
+#     with open("f.txt", "r") as file:
+#         data = file.read()
+#         words = data.split()
+#         count = len(words)
 
-    print("Number of words:", count)
+#     print("Number of words:", count)
 
-except FileNotFoundError:
-    print("File not found")
+# except FileNotFoundError:
+#     print("File not found")
