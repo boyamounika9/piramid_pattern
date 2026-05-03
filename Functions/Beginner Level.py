f5f7cd3e2d37385a4d35f815979f89ef3e079d96
mounika <189mounika.b@gmail.com>
2026-05-03 22:15:19 +0530

    functions

--- Functions/Beginner Level.py
+++ Functions/Beginner Level.py
@@ -1,23 +1,23 @@
-# def greet(name):
-#     print("HELLO,",name,"!")
-# greet("mounika")
+def greet(name):
+    print("HELLO,",name,"!")
+greet("mounika")
 
-# def add(a,b):
-#     return a+b
+def add(a,b):
+    return a+b
 
-# print(add(2,3))
+print(add(2,3))
 
 
-# def evenodd(a):
-#     if a%2==0:
-#         print("even")
-#     else:
-#         print("odd")
-# evenodd(3)
+def evenodd(a):
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+evenodd(3)
 
-# def sq(a):
-#     print(a**2)
-# sq(2)
+def sq(a):
+    print(a**2)
+sq(2)
 
 def power(base,exponent=2):
     print(base**exponent)
