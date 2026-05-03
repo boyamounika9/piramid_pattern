cdb4f57289ddef4805dbad003583d8a142da24a9
mounika <189mounika.b@gmail.com>
2026-05-03 22:12:19 +0530

    function 4

--- Functions/Beginner Level.py
+++ Functions/Beginner Level.py
@@ -8,9 +8,13 @@
 # print(add(2,3))
 
 
-def evenodd(a):
-    if a%2==0:
-        print("even")
-    else:
-        print("odd")
-evenodd(3)
+# def evenodd(a):
+#     if a%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# evenodd(3)
+
+def sq(a):
+    print(a**2)
+sq(2)
