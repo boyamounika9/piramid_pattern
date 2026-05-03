0ad49ce702b0e958a5aa996ca624ff4dbfdcb17e
mounika <189mounika.b@gmail.com>
2026-05-03 23:25:29 +0530

    Exception Handling practice

--- Exception.py
+++ Exception.py
@@ -1,50 +1,50 @@
 
-# try:
-#     a=int(input())
-#     print(type(a))
-# except ValueError:
-#     print("invalid input")
-# finally:
-#     print("sucess")
+try:
+    a=int(input())
+    print(type(a))
+except ValueError:
+    print("invalid input")
+finally:
+    print("sucess")
 
 
-# try:
-#     a=int(input())
-#     b=int(input())
-#     print(a/b)
-# except ZeroDivisionError:
-#     print("cant divisible by zero")
-# except ValueError:
-#     print("invalid input")
-# except Exception as e:
-#     print("unexpected error" ,e)
+try:
+    a=int(input())
+    b=int(input())
+    print(a/b)
+except ZeroDivisionError:
+    print("cant divisible by zero")
+except ValueError:
+    print("invalid input")
+except Exception as e:
+    print("unexpected error" ,e)
 
     
-# try:
-#     a = int(input("Enter first number: "))
-#     b = int(input("Enter second number: "))
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
     
-#     result = a / b
+    result = a / b
 
-# except ZeroDivisionError:
-#     print("Cannot divide by zero")
+except ZeroDivisionError:
+    print("Cannot divide by zero")
 
-# except ValueError:
-#     print("Invalid input")
+except ValueError:
+    print("Invalid input")
 
-# else:
-#     print("Result:", result)
+else:
+    print("Result:", result)
 
 
 
-# try:
-#     age = int(input("Enter age: "))
+try:
+    age = int(input("Enter age: "))
     
-#     if age < 18:
-#         raise ValueError("Age must be 18 or above")
+    if age < 18:
+        raise ValueError("Age must be 18 or above")
 
-# except ValueError as e:
-#     print("Error:", e)
+except ValueError as e:
+    print("Error:", e)
 
-# else:
-#     print("Valid age")
+else:
+    print("Valid age")
